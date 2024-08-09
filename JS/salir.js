d8db8df49faff52d3bcc84e1e6c0b6b3d32fa4ce
HTML5Cpp <68476621+HTML5Cpp@users.noreply.github.com>
2024-08-09 14:10:08 -0600

    Add files via upload

--- JS/salir.js
+++ JS/salir.js
@@ -1,13 +1,25 @@
 let btn_salir_cuenta = document.getElementById('logout');
 
+
 btn_salir_cuenta.addEventListener('click', salir);
 
+if (localStorage.getItem('userID') !== null) {
+    btn_salir_cuenta.classList.remove('noAccount');
+} else {
+    btn_salir_cuenta.classList.add('noAccount');
+}
+
+
+let btn_salir_cuentaMobil = document.getElementById('logoutMobil');
+btn_salir_cuentaMobil.addEventListener('click', salir);
 
 if (localStorage.getItem('userID') !== null) {
-    btn_salir_cuenta.classList.remove('noAccount')
+    btn_salir_cuentaMobil.classList.remove('noAccount');
 } else {
-    btn_salir_cuenta.classList.add('noAccount')
+    btn_salir_cuentaMobil.classList.add('noAccount');
 }
+
+
 
 function salir(){
     localStorage.removeItem('userID');
